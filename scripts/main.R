b6a1f69aa1117f6df6b9ae6016da0a254f86d1bd
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-04-20 17:15:17 +0100

    still have an error because not finding the parameter struct...

---
+++ scripts/main.R
@@ -0,0 +1,27 @@
+library(Rcpp)
+
+sourceCpp("src/GonorrheaDTM.cpp")
+
+inputs <- list(
+  nRaces = 3,
+  nGenders = 2,
+  nSexualBehs = 3,
+  nSexActs = 3,
+  nAges = 101,
+  nDiseaseStates = 10,
+  timeHorizon = 100,
+  ageGroupSize = 5,
+  cDisc = 0.0,
+  uDisc = 0.0)
+
+# birthRate =
+# deathRate =
+# newborneDistribution =
+# transmissionRate =
+# latencyRate =
+# propAsymp =
+# tretmentRate =
+# recoveryRate =
+# screeningRate =
+
+res <- runmodel(inputs)
