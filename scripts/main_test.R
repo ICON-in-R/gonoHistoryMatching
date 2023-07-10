7d78a300bffd8aa61c28762fa297cbef92ffa219
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-07-10 12:28:48 +0100

    emulator running. only giving "implausibile" results so need to tune..

--- scripts/main_test.R
+++ scripts/main_test.R
@@ -2,6 +2,8 @@
 # gonorrhoea transmission dynamic infectious disease model
 # history matching calibration using C++ code
 # https://danny-sc.github.io/determ_workshop/
+# this is a simplified data testing script
+
 Rcpp::compileAttributes()
 
 library(dplyr)
@@ -12,56 +14,59 @@ sourceCpp("src/GonorrheaDTM.cpp", windowsDebugDLL = FALSE)
 
 res <- runmodel()
 
-# original values
-# for testing
-calib_params <- c(0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0,0,0,0,0,0,0,0,
-                  0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0,0,0,0,0,0,0,0,
-                  0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0,0,0,0,0,0,0,0)
+# original values for testing
+calib_params <-
+  c(0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0,0,0,0,0,0,0,0,
+    0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0,0,0,0,0,0,0,0,
+    0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0.022,0,0,0,0,0,0,0,0)
 
-#
+# incidence matrix to vector format
 inc_mat_to_vector <- function(x) {
   x_na <- x[ , colSums(is.na(x)) == 0]
-  c(t(as.matrix(x_na)))
+  # stack years
+  c(as.matrix(x_na))
 }
 
-#' DTM helper 
-#' @param input transmission rate
+#' DTM helper for testing 
+#' 
+#' only change a subset of the input calibration parameters
+#' and return a subset of the output values
+#' for the same groups e.g. age group 1, sexbeh homosexual etc
+#' 
+#' @param input transmission rates, same length as indx
+#' @param indx_in subset input vector index; integer
+#' @param indx_out subset output vector index; integer
 #' @value incidence by group
 #' @examples
-#' out <- get_results(calib_params)
+#' out <- test_get_results(input = c(0.03, 0.03, 0.03))
 #' 
-get_results <- function(input) {
-  write.table(input, file = "Inputs/Calibration parameters.txt",
+test_get_results <- function(input,
+                             indx_in = c(1,2,3,4),
+                             indx_out = c(1,2,3,4)) {
+  params <- scan(file = "Inputs/Calibration parameters0.txt")
+  params[indx_in] <- input
+  write.table(params, file = "Inputs/Calibration parameters.txt",
               col.names = FALSE, row.names = FALSE)
   runmodel()
   out <- read.delim(file = "Inputs/Calibrated incidence.txt", header = FALSE)
   
   # rearrange to calibration format
-  inc_mat_to_vector(out)
+  vout <- inc_mat_to_vector(out)
+  vout[indx_out]
 }
 
-#' only change a subset of the input calibration parameters
-#' and return a subset of the output values
-#' for the same groups e.g. age group 1, sexbeh homosexual etc
-test_get_results <- function(input, indx = c(1,2,3)) {
-  params <- scan(file = "Inputs/Calibration parameters0.txt")
-  params[indx] <- input
-  write.table(params, file = "Inputs/Calibration parameters.txt",
-              col.names = FALSE, row.names = FALSE)
-  runmodel()
-  out <- read.delim(file = "Inputs/Calibrated incidence.txt", header = FALSE)
-  out <- out[indx, ]
-  # rearrange to calibration format
-  inc_mat_to_vector(out)
-}
+###################
+# input group prep
 
-#############
-# input prep
-
-# require
+# create for emulator
 #   wave0: data of inputs and outputs from full model
-#   targets: 
-#   ranges_in: 
+#   ranges_in: upper and lower limits for inputs
+#   targets: output calibration central values and sd
+
+# subset vector
+indx_in <- c(1,2,3,4)
+indx_out <- c(1,2,3,4,      # 2017
+              73,74,75,76)  # 2018
 
 ethnicity_grps <- paste0("e", 1:3)
 sex_grps <- c("male", "female")
@@ -69,6 +74,7 @@ sexbeh_grps <- c("heterosexual", "homosexual", "bisexual")
 age_grps <-  paste0("a", 0:3)
 inc_years <- paste0("y", 2017:2021)
 
+# all combinations of output covariates
 groups_mat <-
   expand.grid(age_grp = age_grps,
               sexbeh = sexbeh_grps,
@@ -76,23 +82,37 @@ groups_mat <-
               ethnicity = ethnicity_grps,
               time = inc_years)
 
-groups_out <- do.call(paste0, groups_mat)
+# vector of all group names
+full_groups_out <- do.call(paste0, groups_mat)
+
+# subset parameters and data for testing
+groups_out <- full_groups_out[indx_out]
 
 n_grps_out <- length(groups_out)
 
+# all combinations of input covariates
+# without year
 groups_in_mat <-
   expand.grid(age_grp = age_grps,
               sexbeh = sexbeh_grps,
               sex = sex_grps,
               ethnicity = ethnicity_grps)
 
-groups_in <- do.call(paste0, groups_in_mat)
+full_groups_in <- do.call(paste0, groups_in_mat)
+
+# subset parameters and data for testing
+groups_in <- full_groups_in[indx_in]
 
 n_grps_in <- length(groups_in)
-ranges_in <- rep(list(c(0, 1)), n_grps_in) |> 
+
+# upper and lower limits for inputs
+##TODO: what upper limit?
+ranges_in <- 
+  rep(list(c(0, 0.1)), n_grps_in) |> 
   setNames(groups_in)
 
-# calibration targets
+#############################
+# create calibration targets
 
 targets_dat <-
   read.delim("Inputs/Calibration targets.txt", sep = "\t", header = FALSE) |> 
@@ -100,18 +120,20 @@ targets_dat <-
 
 # convert to vector
 target_val <- data.frame(
-  val = inc_mat_to_vector(targets_dat),
-  sigma = 0.1)
+  val = inc_mat_to_vector(targets_dat)) |> 
+  mutate(sigma = val/500 + 0.1)            ##TODO: this is arbitrary atm
 
 # convert to list
 targets <-
   split(target_val, 1:nrow(target_val)) |> 
-  setNames(groups_out) |> 
+  setNames(full_groups_out) |> 
   map(as.list)
 
+# subset parameters and data for testing
+targets <- targets[indx_out]
+
 # number of full model simulations
-# n_sim <- n_grps_in*10
-n_sim <- 10
+n_sim <- n_grps_in*10
 
 # latin hypercube design
 # cols: parameters
@@ -124,55 +146,78 @@ for (i in 1:n_grps_in) {
   initial_points[, i] <- lhs_points[, i]*ranges_in[[i]][2] + rdiff 
 }
 
-# test for single input
-# initial_results <- get_results(initial_points[i, ])
+#################
+# run full model
 
-initial_results <- t(apply(initial_points, 1, get_results))
+# test for single input
+# initial_results <- test_get_results(initial_points[i, ], indx_in, indx_out)
+
+initial_results <- t(apply(initial_points, 1,
+                           test_get_results, indx_in = indx_in, indx_out = indx_out))
 
 # all initial values
-wave0 <- cbind(initial_points, initial_results) |> 
+wave0 <-
+  cbind(initial_points, initial_results) |> 
   `colnames<-`(c(groups_in, groups_out)) |> 
   as.data.frame()
+
+save(wave0, file = "Outputs/wave0.RData")
 
 
 ###########
 # emulator
 
+library(ggplot2)
 library(hmer)
 
 ##TODO:...
+
+# load("Outputs/wave0.RData")
 
 ## first wave
 
 ems_wave1 <-
   emulator_from_data(input_data = wave0,
                      output_names = names(targets),
-                     ranges = ranges_in)#, 
+                     ranges = ranges_in,
+                     emulator_type = "deterministic",
+                     order = 1,
                      specified_priors = list(hyper_p = rep(0.55, length(targets))))
 
-emulator_plot(ems_wave1$R200, params = c('beta1', 'gamma'))
+save(ems_wave1, file = "Outputs/ems_wave1.RData")
 
+emulator_plot(ems_wave1$a0heterosexualmalee1y2017)
+emulator_plot(ems_wave1$a1heterosexualmalee1y2017)
+emulator_plot(ems_wave1$a2heterosexualmalee1y2017)
+
+# input-output grid
 plot_actives(ems_wave1)
 
-emulator_plot(ems_wave1$R200, plot_type = 'var', params = c('beta1', 'gamma'))
+# variance
+emulator_plot(ems_wave1$a0heterosexualmalee1y2017, plot_type = 'var')
 
-summary(ems_wave1$R200$model)$adj.r.squared
+summary(ems_wave1$a0heterosexualmalee1y2017$model)$adj.r.squared
 
-emulator_plot(ems_wave1$R200, plot_type = 'imp',
-              targets = targets, params = c('beta1', 'gamma'), cb=TRUE)
+emulator_plot(ems_wave1$a0heterosexualmalee1y2017, plot_type = 'imp',
+              targets = targets, cb=TRUE)
 
+# implausibility >3 unlikely to give a good fit
 emulator_plot(ems_wave1, plot_type = 'imp', 
-              targets = targets, params = c('beta1', 'gamma'), cb=TRUE)
+              targets = targets, cb=TRUE)
+emulator_plot(ems_wave1, plot_type = 'nimp', 
+              targets = targets, cb=TRUE)
 
-space_removed(ems_wave1, targets, ppd=3) + geom_vline(xintercept = 3, lty = 2) + 
+space_removed(ems_wave1, targets, ppd=3) +
+  geom_vline(xintercept = 3, lty = 2) + 
   geom_text(aes(x=3, label="x = 3",y=0.33), colour="black", 
             angle=90, vjust = 1.2, text=element_text(size=11))
 
 ## second wave
 
-ems_wave1_linear <- emulator_from_data(training, names(targets), 
-                                       ranges, quadratic=FALSE,
-                                       specified_priors = list(hyper_p = rep(0.55, length(targets))))
+ems_wave1_linear <-
+  emulator_from_data(training, names(targets), 
+                     ranges, quadratic = FALSE,
+                     specified_priors = list(hyper_p = rep(0.55, length(targets))))
 
 R_squared_linear <- list()
 for (i in 1:length(ems_wave1_linear)) {
@@ -182,20 +227,20 @@ names(R_squared_linear) <- names(ems_wave1_linear)
 unlist(R_squared_linear)
 
 emulator_plot(ems_wave1_linear$I200, plot_type = 'var', 
-              params = c('beta1', 'gamma'))
+              params = c())
 
 emulator_plot(ems_wave1_linear$I200, plot_type = 'imp', targets = targets, 
-              params = c('beta1', 'gamma'), cb=TRUE)
+              params = c(), cb = TRUE)
 
 ems_wave1_linear$I200 <-
   ems_wave1_linear$I20$set_hyperparams(
     list(theta=ems_wave1_linear$I200$corr$hyper_p$theta *3))
 
 emulator_plot(ems_wave1_linear$I200, plot_type = 'var', 
-              params = c('beta1', 'gamma'))
+              params = c())
 
 emulator_plot(ems_wave1_linear$I200, plot_type = 'imp', targets = targets, 
-              params = c('beta1', 'gamma'), cb=TRUE)
+              params = c(), cb=TRUE)
 
 wave_points(list(initial_points, new_points, new_new_points), input_names = names(ranges), p_size=1)
 
